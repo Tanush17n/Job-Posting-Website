2945ca078178e805ba86063748b06c6c98964cf5
Tanush <tanushh17@gmail.com>
2025-02-18 12:08:07 +0530

    Secured MongoDB Credentials

--- server/db.js
+++ server/db.js
@@ -1,5 +1,23 @@
-const mongoose = require("mongoose")
-const url = `mongodb+srv://tanushdb:dbtanush@cluster0.omdmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-module.exports.connect = () => {
-    mongoose.connect(url , console.log("Database is connected"))
+// const mongoose = require("mongoose")
+// const url = `mongodb+srv://tanushdb:dbtanush@cluster0.omdmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// module.exports.connect = () => {
+//     mongoose.connect(url , console.log("Database is connected"))
+// }
+
+const mongoose = require("mongoose");
+require("dotenv").config(); // Load environment variables
+
+const url = process.env.MONGO_URL;
+
+if (!url) {
+    console.error("Error: MONGO_URL is not defined. Make sure you have a .env file and dotenv is installed.");
+    process.exit(1); // Stop the server if the database URL is missing
 }
+
+module.exports.connect = () => {
+    mongoose.connect(url)
+        .then(() => console.log("Database is connected"))
+        .catch((err) => console.error("Database connection error:", err));
+};
+
+
