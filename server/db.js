4775285e36cb4aa5e08945e3ca2b14a39b7ef98c
Tanush <tanushh17@gmail.com>
2025-02-18 12:11:35 +0530

    commit

--- server/db.js
+++ server/db.js
@@ -1,9 +1,3 @@
-// const mongoose = require("mongoose")
-// const url = `mongodb+srv://tanushdb:dbtanush@cluster0.omdmc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-// module.exports.connect = () => {
-//     mongoose.connect(url , console.log("Database is connected"))
-// }
-
 const mongoose = require("mongoose");
 require("dotenv").config(); // Load environment variables
 
